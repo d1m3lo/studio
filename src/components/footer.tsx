1d6f906bfaf28d33375237bc62c94365192a4601
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2025-12-25 21:28:30 +0000

    remova isto (_for element <TwitterIcon>_)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,6 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-const TwitterIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M18 6 6 18" />
-    <path d="m6 6 12 12" />
-  </svg>
-);
-
 const InstagramIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
 );
@@ -38,10 +31,6 @@ export function Footer() {
                 <Link href="#" className="text-foreground/60 hover:text-foreground">
                     <span className="sr-only">Instagram</span>
                     <InstagramIcon className="h-6 w-6" />
-                </Link>
-                <Link href="#" className="text-foreground/60 hover:text-foreground">
-                    <span className="sr-only">Twitter</span>
-                    <TwitterIcon className="h-6 w-6" />
                 </Link>
             </div>
           </div>
