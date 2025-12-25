3bfe832f530004e86d3665b18ba0f325a34825a2
Impulso Digital <contatoimpulsodigitalmg@gmail.com>
2025-12-25 23:34:14 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'API_KEY',
-  authDomain: 'AUTH_DOMAIN',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'STORAGE_BUCKET',
-  messagingSenderId: 'MESSAGING_SENDER_ID',
-  appId: 'APP_ID',
+  apiKey: 'AIzaSyB4wULJMbwd1SB3dpBhFDOReXEl-OdYfek',
+  authDomain: 'studio-9169846239-5049f.firebaseapp.com',
+  projectId: 'studio-9169846239-5049f',
+  storageBucket: 'studio-9169846239-5049f.appspot.com',
+  messagingSenderId: '765736181310',
+  appId: '1:765736181310:web:28b3ab4cd621fd70973b6f',
 };
